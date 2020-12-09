4b739fc23d4e826d97ab0197c2e2b355b6ff62d4
LindaPhi <linda.phinouvong@le-campus-numerique.fr>
2020-12-10 00:18:51 +0100

    Step 3-not finish

--- step1/script.js
+++ step1/script.js
@@ -1,42 +1,76 @@
+/*VARIABLE GLOBALE*/
+var lab =[]; 
+var size; 
+
+/*TAILLE ET CHOIX DU LABY ALÉATOIRE */
 function getRandomIntInclusive(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min +1)) + min;
   }
-
+/* AFFICHE LE LABY*/
  function displayMaze(){
-
+    let b = document.body;
+    let table = document.createElement('table');
+    let div = document.createElement('div');
+    b.append(div);
+    div.append(table);
+    div.setAttribute('id','laby')
     $.get("../labyrinthes.json", function(data){
-        let taille= getRandomIntInclusive(3, 25);
+        size= getRandomIntInclusive(3, 25)
         let ex = getRandomIntInclusive(0, 2);
-        let table = document.createElement('table');
-        let b = document.body;
-        b.append(table);
+        lab = data[size]["ex-"+ex];
 
-        console.log(taille);
-        let lab = data[taille]["ex-"+ex];
-
-        for(i=0; i<taille; i++){
+        for(i=0; i<size; i++){
             let tr = document.createElement('tr');
             table.appendChild(tr);
-                for(y=0; y<taille; y++){   
+            for(y=0; y<size; y++){   
                 let td = document.createElement('td');
                 tr.appendChild(td);
-                if(lab[taille*i+y]["walls"][0]== true){
+
+                if(i==0 && y==0){                        //Première case
+                    td.classList.add('class','first')
+                }
+                if(i==size-1 && y==size-1){              //Dernière case
+                    td.classList.add('class','end')
+                }
+                if(lab[size*i+y]["walls"][0]== true){
                     td.classList.add('class','north')
                 }
-                if(lab[taille*i+y]["walls"][1]== true){
+                if(lab[size*i+y]["walls"][1]== true){
                     td.classList.add('class','east')
                 }
-                if(lab[taille*i+y]["walls"][2]== true){
+                if(lab[size*i+y]["walls"][2]== true){
                     td.classList.add('class','south')
                 }
-                if(lab[taille*i+y]["walls"][3]== true){
+                if(lab[size*i+y]["walls"][3]== true){
                     td.classList.add('class','west')
                 }
-                } 
+            } 
         }
 
     })
 };
+
 displayMaze();
+
+/*PARCOURS LE LABY*/ 
+let tabCaseOver=[];
+function exploreMaze(currentCase, parentCase){  
+    if(lab[currentCase]["walls"][0] == false && currentCase-size != parentCase){
+            exploreMaze(currentCase-size, currentCase);
+    }
+    if(lab[currentCase]["walls"][1] == false && currentCase+1 != parentCase){ 
+            exploreMaze(currentCase+1, currentCase);
+    }
+    if(lab[currentCase]["walls"][2] == false && currentCase+size != parentCase){ 
+            exploreMaze(currentCase+size, currentCase);
+    }
+    if(lab[currentCase]["walls"][3] == false && currentCase-1 != parentCase){ 
+            exploreMaze(currentCase-1, currentCase);
+    }
+
+}
+    
+
+exploreMaze( 0, null);
